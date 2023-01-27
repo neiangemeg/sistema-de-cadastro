cc4f1dfccb31f37e1edd68dec223e31a3ec57c1d
Nei <neiangmeg@uol.com.br>
2023-01-26 23:40:13 -0300

    alterar  console

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 console.log("Rodando sistema de gerenciamneto de jogos")
-console.log("Testes ,,,,    "])
+console.log("Testes ,,,,    ")
 console.log("Testes ,,,,  2  ")
 console.log("Testes ,,,,  3  ")
